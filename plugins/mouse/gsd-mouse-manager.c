4c3c311d42ce8229b76f56393d4df2c31b71b98a
yilang <yilang2007lw@gmail.com>
2013-09-03 15:41:17 +0800

    update syndaemon time from 1.0 to 0.4

--- plugins/mouse/gsd-mouse-manager.c
+++ plugins/mouse/gsd-mouse-manager.c
@@ -583,7 +583,7 @@ set_disable_w_typing (GsdMouseManager *manager, gboolean state)
 
                 g_ptr_array_add (args, "syndaemon");
                 g_ptr_array_add (args, "-i");
-                g_ptr_array_add (args, "1.0");
+                g_ptr_array_add (args, "0.4");
                 //g_ptr_array_add (args, "-t");
                 g_ptr_array_add (args, "-K");
                 g_ptr_array_add (args, "-R");
