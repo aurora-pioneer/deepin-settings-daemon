cf36cc7a49d1682c5cdd9666d9ef44ecc0267087
yilang <yilang2007lw@gmail.com>
2013-09-03 13:11:27 +0800

    fix display idle blank when timeout

--- plugins/power/gsd-power-manager.c
+++ plugins/power/gsd-power-manager.c
@@ -3171,7 +3171,7 @@ idle_blank_cb (GsdPowerManager *manager)
                          idle_mode_to_string (manager->priv->current_idle_mode));
                 return FALSE;
         }
-        //idle_set_mode (manager, GSD_POWER_IDLE_MODE_BLANK);
+        idle_set_mode (manager, GSD_POWER_IDLE_MODE_BLANK);
         return FALSE;
 }
 
